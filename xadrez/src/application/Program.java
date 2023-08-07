73b233549264703d9237a930c63d07d156b5c416
nicolasceccato <nicolasceccato@hotmail.com>
2023-08-07 11:38:49 -0300

    adicionando movimento as pecas

--- xadrez/src/application/Program.java
+++ xadrez/src/application/Program.java
@@ -1,11 +1,29 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+
+        }
     }
 }
